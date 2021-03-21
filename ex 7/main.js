e7da1eb91355ff64c043b5725ae76ec7e7f72481
Denis <denis.cozma2015@gmail.com>
2021-03-22 00:19:33 +0200

    ex 8-10

---
+++ ex 7/main.js
@@ -0,0 +1,21 @@
+/*const age = prompt('introduceti virsta ');
+if (age >= 14 && age <= 90) {
+
+  alert("True");
+
+} else {
+
+  alert("False");
+  
+}*/
+
+
+/*const age = prompt('introduceti virsta ');
+if (!(age >= 14 && age <= 90)) {
+    
+    alert("False");
+
+} else {
+  
+    alert("True");
+}*/
---
+++ ex 9/main.js
@@ -0,0 +1,5 @@
+const name = prompt("care este numele tau?");
+
+alert(`Hello ${name}`);
+
+alert(confirm("Javascript?") ? "bravo" : "ai inceput a studia");
