0be12c203e78949ba0e49e87f0ea4b8e897342c7
Denis <denis.cozma2015@gmail.com>
2021-04-08 00:48:10 +0300

    ex 9

---
+++ main.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body");
+
+const options = {
+    top: "10px",
+    right: "10px",
+    message: "Hello",
+    className: "welcome"
+};
+
+const setStyles = () => {
+    const notificationDiv = document.querySelector(".notification");
+    notificationDiv.style.position = "absolute";
+    notificationDiv.style.display = "block";
+    notificationDiv.style.width = "auto";
+    notificationDiv.style.top = options.top;
+    notificationDiv.style.right = options.right;
+    notificationDiv.style.backgroundColor = "purple";
+};
+
+const showNotification = options => {
+    body.innerHTML = `<div class ="notification ${options.className}"> ${options.message} </div>`;
+    setStyles();
+    setTimeout(() => {
+        const notificationDiv = document.querySelector(".notification");
+        notificationDiv.innerHTML = '';
+    }, 1500)
+};
+
+showNotification(options);
